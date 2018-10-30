213c8dcf0c8013fd5cf17d79dffa38fbde638696
Carfer10 <37177156+Carfer10@users.noreply.github.com>
2018-10-30 23:01:25 +0100

    Normalized and remove Outliers

--- src/milestone1_orientation.py
+++ src/milestone1_orientation.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-# 1. Load the data 
+########################## 1. Load the data ##################################
 # Read the csv
 df = pd.read_csv("..\Data\T2.csv")
 
@@ -25,7 +25,7 @@ df['date'].unique()
 df_2_days = df[(df['TimeStemp'] > '2016-05-03 00:00:00') & (df['TimeStemp'] <= '2016-05-04 23:59:59')]
 print(df_2_days.shape)
 
-
+############################### FILTER DATA ##################################
 # 2. Filter columns
 df_columns_filtered = df_2_days[[c for c in df if (c.startswith('Orientation')and not c.endswith('FFT'))]]
 print(df_columns_filtered.shape)
@@ -40,6 +40,7 @@ for r in range(0, df_columns_filtered.shape[0], 3):
     df_rows_n_columns_filtered = df_rows_n_columns_filtered.append(dataframe)
 
 
+############################## CORRELATION ###################################
 # 4. Correlation
 R = np.corrcoef(np.transpose(df_rows_n_columns_filtered))
 pcolor(R)
@@ -71,14 +72,17 @@ sns.heatmap(R, mask=mask, cmap=cmap, vmax=.8,
  square=True, xticklabels=2, yticklabels=2,
  linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
 
+
+################################## HCA #######################################
 # 5. HCA Feratures
 from sklearn import preprocessing 
 min_max_scaler = preprocessing.MinMaxScaler()
-df_norm = min_max_scaler.fit_transform(df_correlation).transpose()
+df_norm = min_max_scaler.fit_transform(df_correlation)
+df_norm_transposed = df_norm.transpose()
 
 import sklearn.neighbors
 dist = sklearn.neighbors.DistanceMetric.get_metric('euclidean')
-matsim = dist.pairwise(df_norm)
+matsim = dist.pairwise(df_norm_transposed)
 avSim = np.average(matsim)
 print ("%s\t%6.2f" % ('Distancia Media', avSim))
 
@@ -88,16 +92,19 @@ clusters = cluster.hierarchy.linkage(matsim, method = 'complete')
 cluster.hierarchy.dendrogram(clusters, color_threshold=15)
 plt.show()
 
+#####################  PLOT NORMALIZED DATA ##################################
 # 6. Plot the data (Only Orientation as there are 3 features)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.scatter(df_correlation.iloc[:,0], df_correlation.iloc[:,1], df_correlation.iloc[:,2], marker='o', c='red')
+ax.scatter(df_norm[:,0], df_norm[:,1], df_norm[:,2], marker='o', c='red')
 plt.show()
 
+
+########################### CALCULATE PCA ####################################
 # 7. PCA
 from sklearn.decomposition import PCA
 estimator = PCA (n_components = 3)
-X_pca = estimator.fit_transform(df_correlation.transpose())
+X_pca = estimator.fit_transform(df_norm.transpose())
 print(estimator.explained_variance_ratio_) 
 
 df_pca = pd.DataFrame(np.matrix.transpose(estimator.components_), columns=['PC-1', 'PC-2', 'PC-3'])
@@ -119,7 +126,8 @@ ax.grid(True)
 fig.tight_layout()
 plt.show()
 '''
-
+##############################################################################
+############################ KMEANS PCA VALUES ###############################
 # 8. K-means 
 ## 8.1 Setting parameters
 # parameters
@@ -149,13 +157,17 @@ for i in range(2, 11):
     silhouettes.append(metrics.silhouette_score(df_pca, labels))
     
 #Draw distorsion & silhoutte
+plt.subplot(121)
 plt.plot(range(2,11), distortions, marker='o', c='blue')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distorsion')
+plt.subplot(122)
 plt.plot(range(2,11), silhouettes , marker='o', c='green')
 plt.xlabel('Number of clusters')
-plt.ylabel('Silhouette (G) / Distorsion (B)')
+plt.ylabel('Silhouette')
 plt.show()
 
-#8.3 Clustering
+#8.3 Clustering PCA
 k = 8
 
 km = KMeans(k, init, n_init = iterations ,
@@ -173,9 +185,8 @@ print('Distortion: %.2f' % km.inertia_)
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')   
 ax.scatter(df_pca.iloc[:,0], df_pca.iloc[:,1], df_pca.iloc[:,2], marker='o', c=km.labels_)
-ax.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], km.cluster_centers_[:,2], marker='o', c='green')
+ax.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], km.cluster_centers_[:,2], marker='o', c='red')
 plt.show()
-
 
 # 9. Outliers detection (LOF)
 from sklearn.neighbors import LocalOutlierFactor
@@ -192,3 +203,127 @@ ax.scatter(df_pca.iloc[:, 0], df_pca.iloc[:, 1],df_pca.iloc[:,2], s=1000 * radiu
             facecolors='none', label='Outlier scores')
 plt.axis('tight')
 plt.show()
+
+#############################################################################
+##################### KMEANS WITH NORMALIZED DATA ###########################
+distortions = []
+silhouettes = []
+
+for i in range(2, 11):
+    km = KMeans(i, init, n_init = iterations ,max_iter= max_iter, tol = tol,random_state = random_state)
+    labels = km.fit_predict(df_norm)
+    distortions.append(km.inertia_)
+    silhouettes.append(metrics.silhouette_score(df_norm, labels))
+    
+#Draw distorsion & silhoutte
+plt.subplot(121)
+plt.plot(range(2,11), distortions, marker='o', c='blue')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distorsion')
+plt.subplot(122)
+plt.plot(range(2,11), silhouettes , marker='o', c='green')
+plt.xlabel('Number of clusters')
+plt.ylabel('Silhouette')
+plt.show()
+
+k = 8 #Selected value 
+
+km = KMeans(k, init, n_init = iterations ,
+            max_iter= max_iter, tol = tol, random_state = random_state)
+
+y_km = km.fit_predict(df_norm)
+
+from sklearn import metrics
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(df_norm, y_km))
+      
+print('Distortion: %.2f' % km.inertia_)
+
+#plot clusters
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')   
+ax.scatter(df_norm[:,0], df_norm[:,1], df_norm[:,2], marker='o', c=km.labels_)
+ax.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], km.cluster_centers_[:,2], marker='o', c='red')
+plt.show()
+
+#Outlier detection
+from sklearn.neighbors import LocalOutlierFactor
+clf = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
+y_pred = clf.fit_predict(df_norm)
+X_scores = clf.negative_outlier_factor_
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(df_norm[:,0], df_norm[:,1], df_norm[:,2], marker='o')
+# plot circles with radius proportional to the outlier scores
+radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
+ax.scatter(df_norm[:, 0], df_norm[:, 1],df_norm[:,2], s=1000 * radius, edgecolors='r',
+            facecolors='none', label='Outlier scores')
+plt.axis('tight')
+plt.show()
+
+# Outlier deleting (store in a csv)
+rows_deleted = 0
+for s in range(0, len(X_scores)):
+    if X_scores[s-rows_deleted] < -100:
+        df_norm = np.delete(df_norm, (s-rows_deleted), axis=0)
+        X_scores = np.delete(X_scores, (s-rows_deleted), axis=0)
+        rows_deleted+=1
+        
+# Plot results without outliers
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(df_norm[:,0], df_norm[:,1], df_norm[:,2], marker='o')
+# plot circles with radius proportional to the outlier scores
+radius = (X_scores.max() - X_scores) / (X_scores.max() - X_scores.min())
+ax.scatter(df_norm[:, 0], df_norm[:, 1],df_norm[:,2], s=1000 * radius, edgecolors='r',
+            facecolors='none', label='Outlier scores')
+plt.axis('tight')
+plt.show()
+
+# Calculate again value of k
+distortions = []
+silhouettes = []
+
+for i in range(2, 11):
+    km = KMeans(i, init, n_init = iterations ,max_iter= max_iter, tol = tol,random_state = random_state)
+    labels = km.fit_predict(df_norm)
+    distortions.append(km.inertia_)
+    silhouettes.append(metrics.silhouette_score(df_norm, labels))
+    
+#Draw distorsion & silhoutte
+plt.subplot(121)
+plt.plot(range(2,11), distortions, marker='o', c='blue')
+plt.xlabel('Number of clusters')
+plt.ylabel('Distorsion')
+plt.subplot(122)
+plt.plot(range(2,11), silhouettes , marker='o', c='green')
+plt.xlabel('Number of clusters')
+plt.ylabel('Silhouette')
+plt.show()
+
+# Clustering without outliers
+k = 8 #Selected value 
+
+km = KMeans(k, init, n_init = iterations ,
+            max_iter= max_iter, tol = tol, random_state = random_state)
+
+y_km = km.fit_predict(df_norm)
+
+from sklearn import metrics
+print("Silhouette Coefficient: %0.3f"
+      % metrics.silhouette_score(df_norm, y_km))
+      
+print('Distortion: %.2f' % km.inertia_)
+
+#plot clusters
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')   
+ax.scatter(df_norm[:,0], df_norm[:,1], df_norm[:,2], marker='o', c=km.labels_)
+ax.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], km.cluster_centers_[:,2], marker='o', c='red')
+plt.show()
+################################################
+
+
+
+
