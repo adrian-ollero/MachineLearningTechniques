2cd199b9076d3f57d2706ef12fa9268d96b48ffb
Carfer10 <fernando.fvb.fvb@gmail.com>
2018-11-29 17:17:37 +0100

    D2 - Session 2

--- src/milestone2_orientationç.py
+++ src/milestone2_orientationç.py
@@ -16,7 +16,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 ########################## 1. Load the data ##################################
 # Read the csv
-df_sherlock = pd.read_csv(u"./Data/T1.csv")
+df_sherlock = pd.read_csv(u"./Data/T2.csv")
 df_sherlock['UUID'] = df_sherlock['UUID'].astype(np.int64)
 
 df_moriarty = pd.read_csv(u"./Data/Moriarty.csv")
@@ -29,7 +29,7 @@ df_sherlock['Malicious'] =0
 i = 0
 for m_index,m_row in df_moriarty.iterrows():
     for s_index in range(i, df_sherlock.shape[0]):
-        if(df_moriarty.loc[m_index,'UUID'] >= df_sherlock.loc[s_index,'UUID']):
+        if(df_moriarty.loc[m_index,'UUID'] >= df_sherlock.loc[s_index,'UUID'] and s_index < df_sherlock.shape[0]-1):
             if (df_moriarty.loc[m_index,'UUID'] < df_sherlock.loc[s_index+1,'UUID']):
                 if df_moriarty.loc[m_index,'ActionType'] == 'malicious':
                     print("Maligno: "+ str(s_index) + " - " + str(m_index))
