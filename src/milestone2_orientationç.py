4a4b8a5282955b95aefc932b38786e7e167a5659
adrian-ollero <32417194+adrian-ollero@users.noreply.github.com>
2018-12-01 17:17:20 +0100

    Final Steps for Milestone 2

--- src/milestone2_orientationç.py
+++ src/milestone2_orientationç.py
@@ -71,7 +71,7 @@ from sklearn.naive_bayes import GaussianNB
 model = GaussianNB()
 
 model.fit(x_train,y_train)
-y_predG = model.predict(x_test)
+y_pred = model.predict(x_test)
 
 ## Complement  #### NOT AVAILABLE IN THIS VERSION OF THE LIBRARY
 #from sklearn.naive_bayes import ComplementNB
@@ -80,30 +80,15 @@ y_predG = model.predict(x_test)
 #model.fit(x_train,y_train)
 #y_predC = model.predict(x_test)
 
-################################### KNN ######################################
-X = df_norm
-y = columna_malicious
-from matplotlib.colors import ListedColormap
-from sklearn import neighbors, datasets
+############################# METRIC RESULTS #################################
+from sklearn.metrics import classification_report
 
-h = .02  # step size in the mesh
+print("Gaussian Naive-Bayes Results: \n" 
+      +classification_report(y_true=y_test, y_pred=y_pred))
 
-# Create color maps
-n_neighbors = 15
-cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
-cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+# Matriz de confusión
 
-clf = neighbors.KNeighborsClassifier(n_neighbors, weights='uniform')
-clf.fit(X, y)
+print("Matriz de confusión:\n")
+matriz = pd.crosstab(y_test, y_pred, rownames=['actual'], colnames=['preds'])
+print(matriz)
 
-# Plot the decision boundary. For that, we will assign a color to each
-# point in the mesh [x_min, x_max]x[y_min, y_max].
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, h),np.arange(y_min, y_max, h))
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-
-# Put the result into a color plot
-Z = Z.reshape(xx.shape)
-plt.figure()
-plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
